473eaee53b04d5dbbe895d7523fa4b1e8c764c3f
Lada303 <afanaseva.lada@gmail.com>
2022-03-22 14:43:02 +0300

    create repo

---
+++ src/main/java/Lada303/reconstructionApp/convectors/MyCoordinateConvector.java
@@ -0,0 +1,40 @@
+package Lada303.reconstructionApp.convectors;
+/*
+Получает строку и возвращает координыта ячейки в нужном формате - массив из двух целых чисел Х и Y
+Переводит в координаты строки:
+- координаты представляют собой числа из 1 знака от 1..9
+- два числа записаны подря без разделителей
+- два числа разделенных любыми символами
+- если поле 3*3 и ячейки записаны под номерами 1..9
+*/
+
+public class MyCoordinateConvector implements CoordinateConvector {
+
+    public int[] mapCoordinateConvector(String strMapCoordinate) {
+        String str;
+        if (strMapCoordinate.length() == 1) {
+            str = convector1(strMapCoordinate);
+        } else {
+            str = strMapCoordinate.replaceAll("[^0-9]+", "");
+        }
+        int[] xy = new int[2];
+        xy[0] = Integer.parseInt(str.substring(0, 1)) - 1;
+        xy[1] = Integer.parseInt(str.substring(1)) - 1;
+        return xy;
+    }
+
+    private String convector1 (String strMapCoordinate) {
+        switch (strMapCoordinate) {
+            case "1" -> {return "11";}
+            case "2" -> {return "21";}
+            case "3" -> {return "31";}
+            case "4" -> {return "12";}
+            case "5" -> {return "22";}
+            case "6" -> {return "32";}
+            case "7" -> {return "13";}
+            case "8" -> {return "23";}
+            case "9" -> {return "33";}
+        }
+        return "";
+    }
+}
---
+++ src/main/java/Lada303/reconstructionApp/parsers/StaXParser.java
@@ -0,0 +1,118 @@
+package Lada303.reconstructionApp.parsers;
+
+/*
+Осуществлеят парсинг xml-файлов
+Использует StAX
+ */
+
+import Lada303.reconstructionApp.models.Player;
+import Lada303.reconstructionApp.models.Step;
+import org.springframework.stereotype.Component;
+
+import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.events.Attribute;
+import javax.xml.stream.events.StartElement;
+import javax.xml.stream.events.XMLEvent;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+@Component
+public class StaXParser implements Parser{
+
+    private final List<Object> list;
+    private Object element;
+    private XMLEventReader eventReader;
+
+    {
+        list = new ArrayList<>();
+        element = null;
+    }
+
+    @Override
+    public List<Object> readConfig(String configFile) {
+        try {
+            XMLInputFactory inputFactory = XMLInputFactory.newInstance();
+            eventReader = inputFactory.createXMLEventReader(new FileInputStream(configFile));
+
+            while (eventReader.hasNext()) {
+                XMLEvent event = eventReader.nextEvent();
+                if (event.isStartElement()) {
+                    StartElement startElement = event.asStartElement();
+                    readPlayer(startElement);
+                    readMap(startElement);
+                    readStep(startElement);
+                    readResult(startElement);
+                }
+
+                if (event.isEndElement()) {
+                    if (element == null) {
+                        continue;
+                    }
+                    list.add(element);
+                    element = null;
+                }
+            }
+        } catch (FileNotFoundException | XMLStreamException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    private void readPlayer(StartElement startElement) {
+        if (startElement.getName().getLocalPart().equals(GameTag.PLAYER)) {
+            element = new Player();
+            Iterator<Attribute> attributes = startElement.getAttributes();
+            while (attributes.hasNext()) {
+                Attribute attribute = attributes.next();
+                if (attribute.getName().toString().equals(GameTag.PLAYER_ID)) {
+                    ((Player) element).setId(attribute.getValue());
+                }
+                if (attribute.getName().toString().equals(GameTag.PLAYER_NAME)) {
+                    ((Player) element).setName(attribute.getValue());
+                }
+                if (attribute.getName().toString().equals(GameTag.PLAYER_SYMBOL)) {
+                    ((Player) element).setSymbol(attribute.getValue());
+                }
+            }
+        }
+    }
+
+    private void readMap(StartElement startElement) throws XMLStreamException {
+        if (startElement.getName().getLocalPart().equals(GameTag.MAP)) {
+            XMLEvent event = eventReader.nextEvent();
+            element = event.asCharacters().getData();
+        }
+    }
+
+    private void readStep(StartElement startElement) throws XMLStreamException {
+        if (startElement.getName().getLocalPart().equals(GameTag.STEP)) {
+            element = new Step();
+            Iterator<Attribute> attributes = startElement.getAttributes();
+            while (attributes.hasNext()) {
+                Attribute attribute = attributes.next();
+                if (attribute.getName().toString().equals(GameTag.STEP_NUM)) {
+                    ((Step) element).setNum(attribute.getValue());
+                }
+                if (attribute.getName().toString().equals(GameTag.STEP_PLAYER_ID)) {
+                    ((Step) element).setPlayerId(attribute.getValue());
+                }
+            }
+            XMLEvent event = eventReader.nextEvent();
+            ((Step) element).setCellValue(event.asCharacters().getData());
+        }
+    }
+
+    private void readResult(StartElement startElement) throws XMLStreamException {
+        if (startElement.getName().getLocalPart().equals(GameTag.RESULT)) {
+            XMLEvent event = eventReader.nextEvent();
+            if (event.isCharacters()) {
+                element = event.asCharacters().getData();
+            }
+        }
+    }
+}
